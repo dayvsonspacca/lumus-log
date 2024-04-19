297eda6dfc217434d9ea442b247687ff2da4ab4f
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-04-19 08:42:56 -0300

    feat: add parser to format log in a specified format

--- src/lib.rs
+++ src/lib.rs
@@ -2,76 +2,57 @@ use chrono::Local;
 use colored::Colorize;
 
 pub struct ConsoleLogger {
-    verbose: bool,
+    format: String,
 }
 
 impl ConsoleLogger {
-    pub fn new(verbose: bool) -> ConsoleLogger {
-        ConsoleLogger { verbose }
+    pub fn new(format: &str) -> ConsoleLogger {
+        ConsoleLogger {
+            format: String::from(format),
+        }
+    }
+
+    fn parser(&self, message: &str, caller: &str) -> String {
+        let mut parsed_string = String::from(&self.format);
+
+        if self.format.contains("%dt") {
+            let datetime_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+            parsed_string = parsed_string.replace("%dt", &datetime_now);
+        }
+
+        if self.format.contains("%d") {
+            let date_now = Local::now().format("%Y-%m-%d").to_string();
+            parsed_string = parsed_string.replace("%d", &date_now);
+        }
+
+        if self.format.contains("%m") {
+            parsed_string = parsed_string.replace("%m", message);
+        }
+
+        if self.format.contains("%i") {
+            parsed_string = parsed_string.replace("%i", caller);
+        }
+
+        parsed_string
     }
 
     pub fn warn(&self, message: &str) {
-        if self.verbose {
-            let date_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-
-            println!(
-                "{}",
-                format!("[ {} - Warning ] - {}", date_now, message).yellow()
-            );
-        } else {
-            println!("{}", format!("[ Warning ] - {}", message).yellow())
-        }
-    }
-
-    pub fn info(&self, message: &str) {
-        if self.verbose {
-            let date_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-
-            println!(
-                "{}",
-                format!("[ {} - Info ] - {}", date_now, message).cyan()
-            );
-        } else {
-            println!("{}", format!("[ Info ] - {}", message).cyan())
-        }
-    }
-
-    pub fn error(&self, message: &str) {
-        if self.verbose {
-            let date_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-
-            println!(
-                "{}",
-                format!("[ {} - Error ] - {}", date_now, message).red()
-            );
-        } else {
-            println!("{}", format!("[ Error ] - {}", message).red())
-        }
+        println!("{}", self.parser(message, "Warn").yellow());
     }
 
     pub fn log(&self, message: &str) {
-        if self.verbose {
-            let date_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        println!("{}", self.parser(message, "Log").green());
+    }
 
-            println!(
-                "{}",
-                format!("[ {} - Log ] - {}", date_now, message).green()
-            );
-        } else {
-            println!("{}", format!("[ Log ] - {}", message).green())
-        }
+    pub fn info(&self, message: &str) {
+        println!("{}", self.parser(message, "Info").blue());
     }
 
     pub fn debug(&self, message: &str) {
-        if self.verbose {
-            let date_now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        println!("{}", self.parser(message, "Debug").purple());
+    }
 
-            println!(
-                "{}",
-                format!("[ {} - Debug ] - {}", date_now, message).purple()
-            );
-        } else {
-            println!("{}", format!("[ Debug ] - {}", message).purple())
-        }
+    pub fn error(&self, message: &str) {
+        println!("{}", self.parser(message, "Error").red());
     }
 }
