2c5cfbee82a5150392320ccc7eff83cf6d827b02
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-04-19 11:50:13 -0300

    docs: add comments for ConsoleLogger

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,25 @@ pub struct ConsoleLogger {
 }
 
 impl ConsoleLogger {
+    /// Creates a new `ConsoleLogger` instance with the given log format.
+    /// # Example
+    /// ```
+    /// use lumus_logger::ConsoleLogger;
+    /// 
+    /// let logger = ConsoleLogger::new("[ %dt - %i ] - %m");
+    /// logger.warn("Warn message");
+    /// logger.info("Info message");
+    /// logger.log("Log message");
+    /// logger.debug("Debug message");
+    /// logger.error("Error message");
+    /// ```
     pub fn new(format: &str) -> ConsoleLogger {
         ConsoleLogger {
             format: String::from(format),
         }
     }
 
+    /// Logger parser, transform the message in the format specified in new.
     fn parser(&self, message: &str, caller: &str) -> String {
         let mut parsed_string = String::from(&self.format);
 
@@ -36,30 +49,40 @@ impl ConsoleLogger {
         parsed_string
     }
 
+    /// Use `println!()` to output a yellow message in `%m` format str.
+    /// Return `ColoredString`
     pub fn warn(&self, message: &str) -> ColoredString {
         let warn = self.parser(message, "Warn").yellow();
         println!("{}", warn);
         warn
     }
 
+    /// Use `println!()` to output a green message in `%m` format str.
+    /// Return `ColoredString`
     pub fn log(&self, message: &str) -> ColoredString {
         let log = self.parser(message, "Log").green();
         println!("{}", log);
         log
     }
 
+    /// Use `println!()` to output a blue message in `%m` format str.
+    /// Return `ColoredString`
     pub fn info(&self, message: &str) -> ColoredString {
         let info = self.parser(message, "Info").blue();
         println!("{}", info);
         info
     }
 
+    /// Use `println!()` to output a purple message in `%m` format str.
+    /// Return `ColoredString`
     pub fn debug(&self, message: &str) -> ColoredString {
         let debug = self.parser(message, "Debug").purple();
         println!("{}", debug);
         debug
     }
 
+    /// Use `println!()` to output a red message in `%m` format str.
+    /// Return `ColoredString`
     pub fn error(&self, message: &str) -> ColoredString {
         let error = self.parser(message, "Error").red();
         println!("{}", error);
